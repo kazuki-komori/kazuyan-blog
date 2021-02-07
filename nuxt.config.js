aaf08596e9b9b077ad30b03b550ed920c8fe58f9
kazuki-komori <komokazu.nankai@gmail.com>
2021-02-08 00:05:16 +0900

    add: OGP

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     htmlAttrs: {
       lang: "ja",
     },
-    title: "Profill | バーチャル名刺を作成しよう",
+    title: "かずやん| tech blog",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
